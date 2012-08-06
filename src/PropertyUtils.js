78614a8b8cf82c44463aaed8056ed06ce1ee7221
monjudoh <monjudoh@gmail.com>
2012-08-06 19:51:42 +0900

    add PropertyUtils.defineHookableProperty

--- src/PropertyUtils.js
+++ src/PropertyUtils.js
@@ -20,6 +20,7 @@
   }
 })((function(global, undefined) {
   var PropertyUtils = {};
+  PropertyUtils.internalObjectKey = 'PropertyUtils::internalObjectKey';
   /**
    *
    * @param object {Object}
@@ -45,6 +46,85 @@
         });
       },
       configurable : true
+    });
+  };
+  /**
+   *
+   * @param {Object} object
+   * @param {String} key
+   * @return {*}
+   */
+  PropertyUtils.getRaw = function getRaw(object,key) {
+    var internalObjectKey = PropertyUtils.internalObjectKey;
+    if (!object[internalObjectKey]) {
+      object[internalObjectKey] = {};
+    }
+    var internalObject = object[internalObjectKey];
+    return internalObject[key];
+  };
+  /**
+   *
+   * @param {Object} object
+   * @param {String} key
+   * @param {*} val
+   */
+  PropertyUtils.setRaw = function setRaw(object,key,val) {
+    var internalObjectKey = PropertyUtils.internalObjectKey;
+    if (!object[internalObjectKey]) {
+      object[internalObjectKey] = {};
+    }
+    var internalObject = object[internalObjectKey];
+    internalObject[key] = val;
+  };
+  /**
+   *
+   * @param {Object} object
+   * @param {String} key
+   * @param {Object} hooks
+   */
+  PropertyUtils.defineHookableProperty = function defineHookableProperty(object,key,hooks) {
+    /**
+     *
+     * @type {Function}
+     */
+    var beforeGet = hooks.beforeGet;
+    /**
+     *
+     * @type {Function}
+     */
+    var afterGet = hooks.afterGet;
+    /**
+     *
+     * @type {Function}
+     */
+    var beforeSet = hooks.beforeSet;
+    /**
+     *
+     * @type {Function}
+     */
+    var afterSet = hooks.afterSet;
+    Object.defineProperty(object,key,{
+      get : function () {
+        var self = this;
+        if (beforeGet) {
+          beforeGet.call(self);
+        }
+        var val = PropertyUtils.getRaw(self,key);
+        if (afterGet) {
+          val = afterGet.call(self,val);
+        }
+        return val;
+      },
+      set : function (val) {
+        var self = this;
+        if (beforeSet) {
+          val = beforeSet.call(self,val);
+        }
+        PropertyUtils.setRaw(self,key,val);
+        if (afterSet) {
+          afterSet.call(self,val);
+        }
+      }
     });
   };
   return PropertyUtils;
