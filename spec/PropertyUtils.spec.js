e09db744f359458e5cf66b665c95d5560a687dd9
monjudoh <monjudoh@gmail.com>
2012-08-09 17:13:12 +0900

    add PropertyUtils.defineDefaultValueProperty's specs.

--- spec/PropertyUtils.spec.js
+++ spec/PropertyUtils.spec.js
@@ -1,2 +1,36 @@
 describe("PropertyUtils", function() {
+  describe("PropertyUtils.defineDefaultValueProperty", function() {
+    it('define property',function(){
+      var object = Object.create(null);
+      var expectedValue = 1;
+      expect(object['key']).toBeUndefined();
+      PropertyUtils.defineDefaultValueProperty(object,'key',function(){
+        return expectedValue;
+      });
+      expect(object['key']).toEqual(expectedValue);
+    });
+    it('defaultValueGenerator to have been called only after first property access',function(){
+      var spy = jasmine.createSpy();
+      var object = Object.create(null);
+      PropertyUtils.defineDefaultValueProperty(object,'key',function (){
+        spy();
+        return 1;
+      });
+
+      expect(spy).not.toHaveBeenCalled();
+      object['key'];
+      expect(spy).toHaveBeenCalled();
+    });
+    it("defaultValueGenerator's context is saved",function(){
+      var spy = jasmine.createSpy();
+      var object = Object.create(null);
+      PropertyUtils.defineDefaultValueProperty(object,'key',function (){
+        expect(this).toBe(object);
+        spy();
+        return 1;
+      });
+      object['key'];
+      expect(spy).toHaveBeenCalled();
+    })
+  });
 });
