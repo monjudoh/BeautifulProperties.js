48ee71581601ee20492541f5a31aa56d42d18c13
monjudoh <monjudoh@gmail.com>
2015-01-22 17:55:23 +0900

    Add jsdoc.

--- src/Events/HandlerCollection.js
+++ src/Events/HandlerCollection.js
@@ -3,10 +3,21 @@ define('Events/HandlerCollection',[
 ],function (NamespacedKVS) {
   var namespace = 'Events:HandlerCollection';
   var proto = Object.create(null);
+  /**
+   * @function add
+   * @memberOf BeautifulProperties.Events~HandlerCollection#
+   * @param {function} handler
+   * @param {object=} context
+   */
   proto.add = function add(handler,context) {
     this.push(handler);
     this.contexts.push(context);
   };
+  /**
+   * @function remove
+   * @memberOf BeautifulProperties.Events~HandlerCollection#
+   * @param {function} handler
+   */
   proto.remove = function remove(handler) {
     var index;
     while ((index = this.indexOf(handler)) !== -1) {
@@ -14,10 +25,19 @@ define('Events/HandlerCollection',[
       this.contexts.splice(index, 1);
     }
   };
+
+  /**
+   * @function clear
+   * @memberOf BeautifulProperties.Events~HandlerCollection#
+   */
   proto.clear = function clear() {
     this.length = 0;
     this.contexts.length = 0;
   };
+  /**
+   * @function clone
+   * @memberOf BeautifulProperties.Events~HandlerCollection#
+   */
   proto.clone = function clone() {
     var clone = new HandlerCollection;
     var length = this.length;
@@ -27,6 +47,11 @@ define('Events/HandlerCollection',[
     }
     return clone;
   };
+  /**
+   * @constructor BeautifulProperties.Events~HandlerCollection
+   * @extends Array.<function>
+   * @private
+   */
   function HandlerCollection(){
     var self = [];
     Object.keys(proto).forEach(function(key){
