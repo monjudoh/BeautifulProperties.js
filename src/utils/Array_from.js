8a6296e51bb86178278c781d64664b3d168ba400
MURAOKA Yusuke <yusuke@jbking.org>
2016-01-05 16:30:54 +0900

    cleanup a code dup

--- src/utils/Array_from.js
+++ src/utils/Array_from.js
@@ -1,7 +1,7 @@
 define('utils/Array_from',function () {
   var slice = Array.prototype.slice;
-  return function Array_from(arrayLike) {
+  function Array_from(arrayLike) {
     return slice.call(arrayLike);
-  };
+  }
   return Array_from;
 });
